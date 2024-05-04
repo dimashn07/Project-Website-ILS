6ddf66bf176a9e94d44a1c47ea6b88271f68859b
Dimas HN <139949114+dimashn07@users.noreply.github.com>
2024-05-04 11:08:54 +0700

    Update addblog.tsx

--- src/components/Admin/addblog.tsx
+++ src/components/Admin/addblog.tsx
@@ -2,8 +2,11 @@
 
 import React, { useState } from 'react';
 import { EditorState, convertToRaw, ContentState } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+//import { Editor } from 'react-draft-wysiwyg';
+//import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import dynamic from 'next/dynamic';
+
+const Editor = dynamic(() => import('react-draft-wysiwyg').then(mod => mod.Editor), { ssr: false });
 
 const CreateBlogPostForm = () => {
   const [title, setTitle] = useState('');
