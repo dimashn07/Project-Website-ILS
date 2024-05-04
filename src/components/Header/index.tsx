e3e155518af184b12e678dfcf0ea6162929af9d8
Dimas HN <139949114+dimashn07@users.noreply.github.com>
2024-05-04 10:52:10 +0700

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -136,6 +136,8 @@ const Header = () => {
                                 </svg>
                               </span>
                             </p>
+
+                            {menuItem.submenu && (
                             <div
                               className={`submenu relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
                                 openIndex === index ? "block" : "hidden"
@@ -143,7 +145,7 @@ const Header = () => {
                             >
                               {menuItem.submenu.map((submenuItem, index) => (
                                 <Link
-                                  href={submenuItem.path}
+                                  href={submenuItem.path || "/"}
                                   key={index}
                                   className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3"
                                 >
@@ -151,6 +153,7 @@ const Header = () => {
                                 </Link>
                               ))}
                             </div>
+                            )}
                           </>
                         )}
                       </li>
