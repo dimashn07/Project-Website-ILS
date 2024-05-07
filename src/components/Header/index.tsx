669d2b341a7771335e543bea92cf89a0b42695b6
JihanHayaMufialdo <jihannhayaa@gmail.com>
2024-05-07 11:43:58 +0700

    Menambahkan menu layanan

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -136,8 +136,6 @@ const Header = () => {
                                 </svg>
                               </span>
                             </p>
-
-                            {menuItem.submenu && (
                             <div
                               className={`submenu relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
                                 openIndex === index ? "block" : "hidden"
@@ -145,7 +143,7 @@ const Header = () => {
                             >
                               {menuItem.submenu.map((submenuItem, index) => (
                                 <Link
-                                  href={submenuItem.path || "/"}
+                                  href={submenuItem.path}
                                   key={index}
                                   className="block rounded py-2.5 text-sm text-dark hover:text-primary dark:text-white/70 dark:hover:text-white lg:px-3"
                                 >
@@ -153,7 +151,6 @@ const Header = () => {
                                 </Link>
                               ))}
                             </div>
-                            )}
                           </>
                         )}
                       </li>
@@ -162,6 +159,12 @@ const Header = () => {
                 </nav>
               </div>
               <div className="flex items-center justify-end pr-16 lg:pr-0">
+                <Link
+                  href="/signin"
+                  className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 dark:text-white md:block"
+                >
+                  Layanan
+                </Link>
                 <Link
                   href="/signup"
                   className="ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-sm bg-primary px-8 py-3 text-base font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6 xl:px-9"
